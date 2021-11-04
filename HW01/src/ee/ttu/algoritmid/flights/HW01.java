ea99a1f42e7a5eb98b2cb84e7dc018d4a7675e74
Liisi Nõojärv <linooj@ttu.ee>
2021-11-04 13:32:09 +0200

    mall

---
+++ HW01/src/ee/ttu/algoritmid/flights/HW01.java
@@ -0,0 +1,18 @@
+package ee.ttu.algoritmid.flights;
+
+import java.util.List;
+
+public class HW01 implements FlightCrewRegistrationSystem {
+
+    @Override
+    public FlightCrew registerToFlight(FlightCrewMember participant) throws IllegalArgumentException {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public List<FlightCrewMember> crewMembersWithoutTeam() {
+        // TODO
+        return null;
+    }
+}
